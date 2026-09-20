276fd8fa179fd80cf422a3f7f20382930e4894ed
EliteMay <k12m45k@gmail.com>
2026-09-20 21:51:20 +0900

    Fix Electron sandbox preload initialization
    * Fix sandboxed preload loader
    
    * Add sandbox-compatible CommonJS preload
    
    * Remove unsupported sandboxed ESM preload
    
    * Guard sandbox preload compatibility
    
    * Document sandbox preload requirement

---
+++ desktop/preload.cjs
@@ -0,0 +1,17 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("localAI", {
+  getSettings: () => ipcRenderer.invoke("settings:get"),
+  saveSettings: (input) => ipcRenderer.invoke("settings:save", input),
+  selectRepository: () => ipcRenderer.invoke("repository:select"),
+  runCommand: (input) => ipcRenderer.invoke("command:run", input),
+  cancelCommand: () => ipcRenderer.invoke("command:cancel"),
+  listHistory: () => ipcRenderer.invoke("history:list"),
+  readRunResult: (id) => ipcRenderer.invoke("history:result", id),
+  copyText: (text) => ipcRenderer.invoke("clipboard:write", text),
+  onLog: (callback) => {
+    const handler = (_event, payload) => callback(payload);
+    ipcRenderer.on("command:log", handler);
+    return () => ipcRenderer.removeListener("command:log", handler);
+  }
+});
